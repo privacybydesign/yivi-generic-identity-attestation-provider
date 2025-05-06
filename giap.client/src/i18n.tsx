f79f475dec8bef1fa7f84899683aafb24350d634
Quincy de Jong <232e7911-6b9f-4213-8b52-1e59762e341d@slmail.me>
2025-05-06 16:35:49 +0200

    feat: add language based routing based on IBAN issuer

---
+++ giap.client/src/i18n.tsx
@@ -0,0 +1,35 @@
+import i18n from 'i18next';
+import {initReactI18next} from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+
+i18n
+    // detect user language
+    // learn more: https://github.com/i18next/i18next-browser-languageDetector
+    .use(LanguageDetector)
+    // pass the i18n instance to react-i18next.
+    .use(initReactI18next)
+    // init i18next
+    // for all options read: https://www.i18next.com/overview/configuration-options
+    .init({
+        debug: true,
+        fallbackLng: 'en',
+        interpolation: {
+            escapeValue: false, // not needed for React as it escapes by default
+        },
+        resources: {
+            en: {
+                translation: {
+                    login: "Log in to load attributes",
+                    info: "Attributes can be loaded into your Yivi app. To load these attributes, it is necessary that you log in using the button below and if prompted give Yivi access to your profile data. We use this data only during attribute loading."
+                }
+            },
+            nl: {
+                translation: {
+                    login: "Log in om attributen te laden",
+                    info: "Attributen kunnen eenvoudig in de Yivi app geladen worden. Hiervoor dient u in te loggen in via de knop hieronder en eventueel toestemming te geven. We gebruiken deze gegevens alleen tijdens het laden van de attributen in de app."
+                }
+            }
+        }
+    });
+
+export default i18n;
--- giap.client/src/main.tsx
+++ giap.client/src/main.tsx
@@ -1,10 +1,35 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import {StrictMode, useEffect} from 'react'
+import {createRoot} from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import {BrowserRouter, Routes, Route, useParams} from 'react-router';
+import {useTranslation} from 'react-i18next';
+
+// Wrapper that sets the language based on the URL
+// from IBAN issuer: https://github.com/privacybydesign/yivi-iban-issuer/tree/main/react-cra
+function LanguageRouter() {
+    const {lang} = useParams();
+    const {i18n} = useTranslation();
+
+    useEffect(() => {
+        if (lang && i18n.language !== lang) {
+            i18n.changeLanguage(lang);
+        }
+    }, [lang, i18n]);
+
+    return (
+        <Routes>
+            <Route path="/:slug" element={<App/>}/>
+        </Routes>
+    );
+}
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <StrictMode>
+        <BrowserRouter>
+            <Routes>
+                <Route path=":lang/*" element={<LanguageRouter/>}/>
+            </Routes>
+        </BrowserRouter>
+    </StrictMode>,
 )
