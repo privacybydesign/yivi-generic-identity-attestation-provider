5267a1566008f2ef7c0185c2f4a75908b7971b2f
Quincy de Jong <232e7911-6b9f-4213-8b52-1e59762e341d@slmail.me>
2025-05-07 19:06:26 +0200

    test: add initial IdentityProviderService tests

---
+++ GIAP.Tests/IdentityProviderServiceTests.cs
@@ -0,0 +1,165 @@
+using GIAP.Server.Services;
+using Shouldly;
+using NSubstitute;
+
+namespace GIAP.Tests;
+
+public class IdentityProviderServiceTests
+{
+    /**
+     * Given no identity providers file
+     * When the service is initialized,
+     * Then an exception is thrown
+     */
+    [Fact]
+    public void NoIdentityProviderJsonFound()
+    {
+        var fileSystem = Substitute.For<IFileSystem>();
+        var expectedPath = Path.Combine(
+            AppContext.BaseDirectory,
+            "Configuration",
+            "identity-providers.json"
+        );
+
+        fileSystem.Exists(expectedPath).Returns(false);
+
+        var service = new IdentityProviderService(fileSystem);
+
+        Should.Throw<FileNotFoundException>(() => service.Initialize()).Message
+            .ShouldBe("Identity providers file not found");
+    }
+
+    /**
+     * Given an empty identity providers file
+     * When the service is initialized,
+     * Then an exception is thrown
+     */
+    [Fact]
+    public void NoIdentityProvidersFound()
+    {
+        var fileSystem = Substitute.For<IFileSystem>();
+        var expectedPath = Path.Combine(
+            AppContext.BaseDirectory,
+            "Configuration",
+            "identity-providers.json"
+        );
+
+        fileSystem.Exists(expectedPath).Returns(true);
+        fileSystem.ReadAllText(expectedPath).Returns("[]");
+
+        var service = new IdentityProviderService(fileSystem);
+        Should.Throw<InvalidOperationException>(() => service.Initialize()).Message
+            .ShouldBe("No identity providers found");
+    }
+
+
+    /**
+     * Given a JSON identity providers file with missing keys
+     * When the service is initialized,
+     * Then an exception is thrown
+     */
+    [Fact]
+    public void IdentityProviderJsonWithMissingKeys()
+    {
+        var fileSystem = Substitute.For<IFileSystem>();
+        var expectedPath = Path.Combine(
+            AppContext.BaseDirectory,
+            "Configuration",
+            "identity-providers.json"
+        );
+
+        fileSystem.Exists(expectedPath).Returns(true);
+
+        // JSON doesn't include the required name key from the IdentityProvider model
+        const string json = """
+                            [
+                                {
+                                  "slug": "example-1"
+                                },
+                                {
+                                  "slug": "example-2"
+                                }
+                            ]
+                            """;
+
+        fileSystem.ReadAllText(expectedPath).Returns(json);
+
+        var service = new IdentityProviderService(fileSystem);
+
+        Should.Throw<InvalidDataException>(() => service.Initialize()).Message
+            .ShouldBe("Invalid JSON and/or missing keys in identity providers file");
+    }
+
+    /**
+     * Given a malformed JSON identity providers file
+     * When the service is initialized,
+     * Then an exception is thrown
+     */
+    [Fact]
+    public void MalformedIdentityProviderJson()
+    {
+        var fileSystem = Substitute.For<IFileSystem>();
+        var expectedPath = Path.Combine(
+            AppContext.BaseDirectory,
+            "Configuration",
+            "identity-providers.json"
+        );
+
+        fileSystem.Exists(expectedPath).Returns(true);
+
+        const string json = """
+                            [
+                                {
+                                  "name": "Example Identity Provider 1",
+                                  "slug": "example-1"
+                                },
+                                {
+                                  "name": "Example
+                            """;
+
+        fileSystem.ReadAllText(expectedPath).Returns(json);
+
+        var service = new IdentityProviderService(fileSystem);
+
+        Should.Throw<InvalidDataException>(() => service.Initialize()).Message
+            .ShouldBe("Invalid JSON and/or missing keys in identity providers file");
+    }
+
+    /**
+     * Given a valid identity providers file
+     * When the service is initialized,
+     * Then no exception is thrown
+     */
+    [Fact]
+    public void JsonMeetsRequirements()
+    {
+        var fileSystem = Substitute.For<IFileSystem>();
+        var expectedPath = Path.Combine(
+            AppContext.BaseDirectory,
+            "Configuration",
+            "identity-providers.json"
+        );
+
+        fileSystem.Exists(expectedPath).Returns(true);
+
+        const string json = """
+                            [
+                                {
+                                  "name": "Example Identity Provider 1",
+                                  "slug": "example-1"
+                                },
+                                {
+                                  "name": "Example Identity Provider 2",
+                                  "slug": "example-2"
+                                }
+                            ]
+                            """;
+
+        fileSystem.ReadAllText(expectedPath).Returns(json);
+
+        var service = new IdentityProviderService(fileSystem);
+        Should.NotThrow(() => service.Initialize());
+        service.GetBySlug("example-1")?.Name.ShouldBe("Example Identity Provider 1");
+        service.GetBySlug("example-2")?.Name.ShouldBe("Example Identity Provider 2");
+    }
+}
