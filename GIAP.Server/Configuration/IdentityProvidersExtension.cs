500492fa5b0e9d07430838a8ae75011949b74893
Quincy de Jong <232e7911-6b9f-4213-8b52-1e59762e341d@slmail.me>
2025-06-10 08:26:54 +0200

    feat: add IdentityProvidersExtension

---
+++ GIAP.Server/Configuration/IdentityProvidersExtension.cs
@@ -0,0 +1,39 @@
+﻿using GIAP.Server.Services;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+
+namespace GIAP.Server.Configuration;
+
+/// <summary>
+/// Extension method for adding identity providers to the authentication builder.
+/// </summary>
+public static class IdentityProvidersExtension
+{
+    /// <summary>
+    /// Adds the identity providers from the IdentityProviderService to the authentication builder.
+    /// </summary>
+    /// <param name="builder">Builder to add identity providers using AddOpenIdConnect.</param>
+    /// <returns>The AuthenticationBuilder builder with all the configured identity providers.</returns>
+    public static AuthenticationBuilder AddIdentityProviders(this AuthenticationBuilder builder)
+    {
+        var identityProviderService =
+            builder.Services.BuildServiceProvider().GetRequiredService<IdentityProviderService>();
+        var identityProviders = identityProviderService.GetAll();
+
+        foreach (var identityProvider in identityProviders)
+        {
+            builder.AddOpenIdConnect(identityProvider.Slug, configure =>
+            {
+                configure.MetadataAddress = identityProvider.OpenIdWellKnownUrl;
+                configure.ClientId = identityProvider.ClientId;
+                configure.ClientSecret = identityProvider.ClientSecret;
+                configure.CallbackPath = identityProvider.CallbackPath;
+                configure.ResponseType = OpenIdConnectResponseType.Code;
+                configure.SaveTokens = true; // necessary to get the access token for optional API calls
+                configure.GetClaimsFromUserInfoEndpoint = true; // necessary to get the user info from the IdP
+            });
+        }
+
+        return builder;
+    }
+}
--- GIAP.Server/Program.cs
+++ GIAP.Server/Program.cs
@@ -1,16 +1,42 @@
+using GIAP.Server.Configuration;
 using GIAP.Server.Services;
+using Microsoft.AspNetCore.Authentication.Cookies;
 
 var builder = WebApplication.CreateBuilder(args);
 
+DotNetEnv.Env.Load();
+
 // Add services to the container.
-builder.Services.AddSingleton<IIdentityProviderService, IdentityProviderService>();
+builder.Services.AddTransient<AttributeMapperService>();
+builder.Services.AddTransient<CredentialAttributeService>();
+
 builder.Services.AddSingleton<IFileSystem, FileSystem>();
+builder.Services.AddSingleton<IdentityProviderService>(serviceProvider =>
+{
+    var fileSystem = serviceProvider.GetRequiredService<IFileSystem>();
+    var identityProviderService = new IdentityProviderService(fileSystem);
+    // Needs to be initialized immediately after creation because AddIdentityProviders depends on it.
+    identityProviderService.Initialize();
+    return identityProviderService;
+});
+
+builder.Services.AddHttpClient<ApiClient>();
+builder.Services.AddHttpClient<SchemeCredentialClient>();
+builder.Services.AddHttpClient<IrmaServerClient>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddHealthChecks();
+
+builder.Services
+    .AddAuthentication(configure =>
+    {
+        configure.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme; // authenticate using cookies
+    })
+    .AddCookie()
+    .AddIdentityProviders();
 
 var app = builder.Build();
 
@@ -24,13 +50,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseAuthorization();
-
 app.MapControllers();
 app.MapHealthChecks("/health");
 app.MapFallbackToFile("/index.html");
-
-var identityProviderService = app.Services.GetRequiredService<IIdentityProviderService>();
-identityProviderService.Initialize();
 
 app.Run();
