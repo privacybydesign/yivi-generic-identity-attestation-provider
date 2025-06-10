db8ab49f8b7fd76159635bd169c773281b8f6b86
Quincy de Jong <232e7911-6b9f-4213-8b52-1e59762e341d@slmail.me>
2025-06-10 08:04:37 +0200

    chore: remove unnecessary interface for IdentityProviderService

--- GIAP.Server/Services/IIdentityProviderService.cs
+++
@@ -1,9 +0,0 @@
-﻿using GIAP.Server.Models;
-
-namespace GIAP.Server.Services;
-
-public interface IIdentityProviderService
-{
-    void Initialize();
-    IdentityProvider? GetBySlug(string slug);
-}
