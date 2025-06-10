bdc183f91821a5f963eff09f37437a00e70b8541
Quincy de Jong <232e7911-6b9f-4213-8b52-1e59762e341d@slmail.me>
2025-06-10 08:05:34 +0200

    docs: add comments for the FileSystem classes

--- GIAP.Server/Services/FileSystem.cs
+++ GIAP.Server/Services/FileSystem.cs
@@ -1,12 +1,17 @@
 ﻿namespace GIAP.Server.Services;
 
+/// <summary>
+/// This uses the System.IO.File class to interact with files.
+/// </summary>
 public class FileSystem : IFileSystem
 {
+    /// <inheritdoc/>
     public bool Exists(string path)
     {
         return File.Exists(path);
     }
-    
+
+    /// <inheritdoc/>
     public string ReadAllText(string path)
     {
         return File.ReadAllText(path);
--- GIAP.Server/Services/IFileSystem.cs
+++ GIAP.Server/Services/IFileSystem.cs
@@ -1,7 +1,22 @@
 ﻿namespace GIAP.Server.Services;
 
+/// <summary>
+/// Interface for file system operations.
+/// Necessary for mocking in tests.
+/// </summary>
 public interface IFileSystem
 {
+    /// <summary>
+    /// Check if a file exists at the specified path.
+    /// </summary>
+    /// <param name="path">The path where the file is stored.</param>
+    /// <returns>Either true or false depending on if the file exists.</returns>
     bool Exists(string path);
+
+    /// <summary>
+    /// Read all text from a file at the specified path.
+    /// </summary>
+    /// <param name="path">The path where the file is stored.</param>
+    /// <returns>Returns the text from the specified file.</returns>
     string ReadAllText(string path);
 }
