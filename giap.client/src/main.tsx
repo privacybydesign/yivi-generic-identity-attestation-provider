f8f57dee39f5e21d636a5f0fe91cfbbe42a69011
Quincy de Jong <232e7911-6b9f-4213-8b52-1e59762e341d@slmail.me>
2025-06-10 10:34:50 +0200

    feat: add routing to attributes and success page

--- giap.client/src/main.tsx
+++ giap.client/src/main.tsx
@@ -6,7 +6,9 @@ import {useTranslation} from 'react-i18next';
 import "./i18n";
 import './index.css';
 
-import LoginPage from './pages/LoginPage';
+import Login from './pages/Login.tsx';
+import Issuance from './pages/Issuance.tsx';
+import Done from "./pages/Done.tsx";
 
 // Wrapper that sets the language based on the URL
 // from IBAN issuer: https://github.com/privacybydesign/yivi-iban-issuer/tree/main/react-cra
@@ -22,7 +24,9 @@ function LanguageRouter() {
 
     return (
         <Routes>
-            <Route path="/:slug" element={<LoginPage/>}/>
+            <Route path=":slug" element={<Login/>}/>
+            <Route path=":slug/load-attributes" element={<Issuance/>}/>
+            <Route path=":slug/success" element={<Done/>}/>
         </Routes>
     );
 }
