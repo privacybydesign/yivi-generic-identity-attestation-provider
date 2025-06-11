fb5143657d9ed8723641a25898c36d01e2619a3a
Quincy de Jong <232e7911-6b9f-4213-8b52-1e59762e341d@slmail.me>
2025-06-11 16:22:40 +0200

    feat: add IAttributeMapperService for testing

--- GIAP.Server/Services/AttributeMapperService.cs
+++ GIAP.Server/Services/AttributeMapperService.cs
@@ -3,14 +3,9 @@
 /// <summary>
 /// This service is responsible for mapping and filtering attributes for credentials.
 /// </summary>
-public class AttributeMapperService
+public class AttributeMapperService : IAttributeMapperService
 {
-    /// <summary>
-    /// Initializes the attributes for a credential with JWT claims, optionally combined with API data.
-    /// </summary>
-    /// <param name="jwtClaims">JWT Claims consists of ids with values</param>
-    /// <param name="apiData">API data consists of ids with values</param>
-    /// <returns>One dictionary with JWT claims, optionally combined with API data</returns>
+    /// <inheritdoc/>
     public Dictionary<string, string> InitializeAttributes(
         Dictionary<string, string> jwtClaims,
         Dictionary<string, string>? apiData
@@ -21,17 +16,7 @@ public class AttributeMapperService
             : CombineJwtClaimsAndApiData(jwtClaims, apiData);
     }
 
-    /// <summary>
-    /// Filters and maps the attributes based on identityProviderAttributeMapping.
-    /// It skips the attributes that are not in the identityProviderAttributeMapping.
-    /// </summary>
-    /// <remarks>
-    /// It assumes that the keys in identityProviderAttributeMapping are the keys received from the identity provider
-    /// or from a resource server (API), and it assumes the values in identityProviderAttributeMapping are the scheme IDs.
-    /// </remarks>
-    /// <param name="attributes">Attributes that aren't filtered or mapped to scheme IDs yet</param>
-    /// <param name="identityProviderAttributeMapping">Dictionary keys are IdP/API keys and values are scheme IDs</param>
-    /// <returns>Returns a dictionary with keys as scheme IDs and the values from the attribute dictionary.</returns>
+    /// <inheritdoc/>
     public Dictionary<string, string> FilterAndMapByConfig(
         Dictionary<string, string> attributes,
         Dictionary<string, string> identityProviderAttributeMapping
@@ -51,12 +36,7 @@ public class AttributeMapperService
         return credentialAttributes;
     }
 
-    /// <summary>
-    /// Replaces id's in a credential with human-readable names for display purposes.
-    /// </summary>
-    /// <param name="credentialAttributes">A dictionary with the keys and values of the credential.</param>
-    /// <param name="schemeAttributes">The scheme attributes with keys as IDs and values as the human-readable names.</param>
-    /// <returns>A dictionary with keys as human-readable names and attribute values.</returns>
+    /// <inheritdoc/>
     public Dictionary<string, string> MapToHumanReadableAttributes(
         Dictionary<string, string> credentialAttributes,
         Dictionary<string, string> schemeAttributes)
---
+++ GIAP.Server/Services/IAttributeMapperService.cs
@@ -0,0 +1,42 @@
+﻿namespace GIAP.Server.Services;
+
+public interface IAttributeMapperService
+{
+    /// <summary>
+    /// Initializes the attributes for a credential with JWT claims, optionally combined with API data.
+    /// </summary>
+    /// <param name="jwtClaims">JWT Claims consists of ids with values</param>
+    /// <param name="apiData">API data consists of ids with values</param>
+    /// <returns>One dictionary with JWT claims, optionally combined with API data</returns>
+    Dictionary<string, string> InitializeAttributes(
+        Dictionary<string, string> jwtClaims,
+        Dictionary<string, string>? apiData
+    );
+
+    /// <summary>
+    /// Filters and maps the attributes based on identityProviderAttributeMapping.
+    /// It skips the attributes that are not in the identityProviderAttributeMapping.
+    /// </summary>
+    /// <remarks>
+    /// It assumes that the keys in identityProviderAttributeMapping are the keys received from the identity provider
+    /// or from a resource server (API), and it assumes the values in identityProviderAttributeMapping are the scheme IDs.
+    /// </remarks>
+    /// <param name="attributes">Attributes that aren't filtered or mapped to scheme IDs yet</param>
+    /// <param name="identityProviderAttributeMapping">Dictionary keys are IdP/API keys and values are scheme IDs</param>
+    /// <returns>Returns a dictionary with keys as scheme IDs and the values from the attribute dictionary.</returns>
+    Dictionary<string, string> FilterAndMapByConfig(
+        Dictionary<string, string> attributes,
+        Dictionary<string, string> identityProviderAttributeMapping
+    );
+
+    /// <summary>
+    /// Replaces id's in a credential with human-readable names for display purposes.
+    /// </summary>
+    /// <param name="credentialAttributes">A dictionary with the keys and values of the credential.</param>
+    /// <param name="schemeAttributes">The scheme attributes with keys as IDs and values as the human-readable names.</param>
+    /// <returns>A dictionary with keys as human-readable names and attribute values.</returns>
+    Dictionary<string, string> MapToHumanReadableAttributes(
+        Dictionary<string, string> credentialAttributes,
+        Dictionary<string, string> schemeAttributes
+    );
+}
