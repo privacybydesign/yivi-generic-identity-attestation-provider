20b5bdd481af77323420881575b675c8578973b5
Quincy de Jong <232e7911-6b9f-4213-8b52-1e59762e341d@slmail.me>
2025-06-11 16:29:02 +0200

    feat: add ICredentialAttributeService for testing

---
+++ GIAP.Server/Services/ICredentialAttributeService.cs
@@ -0,0 +1,36 @@
+﻿namespace GIAP.Server.Services;
+
+public interface ICredentialAttributeService
+{
+    /// <summary>
+    /// Get credential attributes for issuance based on the attribute mapping.
+    /// </summary>
+    /// <param name="idpAttributesMapping">The attribute mapping from the IdentityProvider configuration.</param>
+    /// <param name="jwtClaims">JWT claims from the IdentityProvider</param>
+    /// <param name="apiData">API data from the API</param>
+    /// <returns>Returns the credential attributes for issuance.</returns>
+    Dictionary<string, string> GetCredentialAttributesForIssuance(
+        Dictionary<string, string> idpAttributesMapping,
+        Dictionary<string, string> jwtClaims,
+        Dictionary<string, string>? apiData
+    );
+
+    /// <summary>
+    /// Get credential attributes for display based on the scheme attributes, IdP mapping, JWT claims, and API data.
+    /// </summary>
+    /// <remarks>
+    /// This requires the scheme attributes for the human-readable names of the attributes.
+    /// Rather than mapping the keys of the dictionary to IDs, it maps it to human-readable names.
+    /// </remarks>
+    /// <param name="schemeAttributes">The scheme attributes with keys as IDs and values as the human-readable names.</param>
+    /// <param name="idpAttributesMapping">The attribute mapping from the IdentityProvider configuration.</param>
+    /// <param name="jwtClaims">JWT Claims from</param>
+    /// <param name="apiData">The API data from the API</param>
+    /// <returns>Returns a dictionary with keys as human-readable names and attribute values.</returns>
+    Dictionary<string, string> GetCredentialAttributesForDisplay(
+        Dictionary<string, string> schemeAttributes,
+        Dictionary<string, string> idpAttributesMapping,
+        Dictionary<string, string> jwtClaims,
+        Dictionary<string, string>? apiData
+    );
+}
