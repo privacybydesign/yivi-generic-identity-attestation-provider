dbad318cc9af4869fa59b73178bfcd5e9956808f
Quincy de Jong <232e7911-6b9f-4213-8b52-1e59762e341d@slmail.me>
2025-07-01 15:23:38 +0200

    test: add missing all environment variables test

---
+++ GIAP.Tests/EnvVariablesTests.cs
@@ -0,0 +1,16 @@
+﻿using GIAP.Server.Services;
+using Shouldly;
+
+namespace GIAP.Tests;
+
+public class EnvVariablesTests
+{
+    [Fact]
+    public void Constructor_MissingAllEnvironmentVariables_ThrowsException()
+    {
+        // assume no environment variables are set when this test is run 
+
+        Should.Throw<InvalidDataException>(() => new EnvVariables()).Message.ShouldBe(
+            "Not all required environment variables are set. Check the documentation README.md for the required environment variables. Make sure the .env is located at GIAP.server/.env. The following environment variables are missing: SCHEME_BASE_URL, SCHEME_NAME, IRMA_SERVER_BASE_URL, IRMA_SERVER_API_TOKEN.");
+    }
+}
