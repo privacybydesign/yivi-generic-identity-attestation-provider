0c4f44d8864a14586dad30666cd14dba388e871d
Quincy de Jong <22809491+quincydejong@users.noreply.github.com>
2025-07-03 15:26:42 +0200

    feat: set document title based on identity provider name (#15)

--- giap.client/src/pages/Done.tsx
+++ giap.client/src/pages/Done.tsx
@@ -1,8 +1,18 @@
 import done from '../assets/images/done.png'
 import {useTranslation} from "react-i18next";
+import {useEffect} from "react";
+import {useLocation} from "react-router";
 
 function Done() {
     const {t} = useTranslation();
+    const location = useLocation();
+    const identityProviderName = location.state?.identityProviderName || "";
+
+    useEffect(() => {
+        if (identityProviderName) {
+            document.title = identityProviderName;
+        }
+    }, [identityProviderName]);
 
     return <>
         <header>
